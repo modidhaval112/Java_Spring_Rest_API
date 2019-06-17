12a844a353084a792793a956b6f8fe33062f0959
modidhaval112 <modidhaval112@gmail.com>
2019-06-16 20:10:32 -0400

    Added unit test cases for different operations

--- src/test/java/test/com/h2rd/refactoring/unit/UserResourceUnitTest.java
+++ src/test/java/test/com/h2rd/refactoring/unit/UserResourceUnitTest.java
@@ -1,12 +1,13 @@
 package test.com.h2rd.refactoring.unit;
 
-import com.h2rd.refactoring.usermanagement.User;
-import com.h2rd.refactoring.usermanagement.UserDao;
-import com.h2rd.refactoring.web.UserResource;
+import javax.ws.rs.core.Response;
+
 import junit.framework.Assert;
+
 import org.junit.Test;
 
-import javax.ws.rs.core.Response;
+import com.h2rd.refactoring.usermanagement.UserDao;
+import com.h2rd.refactoring.web.UserResource;
 
 public class UserResourceUnitTest {
 
@@ -14,17 +15,200 @@ public class UserResourceUnitTest {
     UserDao userDao;
 
     @Test
-    public void getUsersTest() {
-
+    public void getUsersPositiveTest() {
+    	
         userResource = new UserResource();
-        userDao = UserDao.getUserDao();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
 
-        User user = new User();
-        user.setName("fake user");
-        user.setEmail("fake@user.com");
-        userDao.saveUser(user);
+        userResource.addUser("fake user", "fake@user.com", "admin,master");
 
         Response response = userResource.getUsers();
         Assert.assertEquals(200, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void getUsersNotPresentNegativeTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+
+        Response response = userResource.getUsers();
+        Assert.assertEquals(400, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void addUserPositiveTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+
+        Response response = userResource.addUser("fake user", "fake@user.com", "admin,master");
+        Assert.assertEquals(200, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void addUserEmailExistNegativeTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+        userResource.addUser("fake user", "fake@user.com", "admin,master");
+        Response response = userResource.addUser("fake user", "fake@user.com", "admin,master");
+        Assert.assertEquals(400, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void addUserRoleEmptyNegativeTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+        Response response = userResource.addUser("fake user", "fake@user.com", "");
+        Assert.assertEquals(400, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void updateUserPositiveTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+        
+        userResource.addUser("fake user", "fake@user.com", "admin,master");
+        Response response = userResource.updateUser("Real user", "fake@user.com", "ceo,cfo");
+        Assert.assertEquals(200, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void updateUserNotFoundNegativeTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+        
+        Response response = userResource.updateUser("Real user", "fake@user.com", "ceo,cfo");
+        Assert.assertEquals(400, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void updateUserNoRoleProvidedNegativeTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+        userResource.addUser("fake user", "fake@user.com", "admin,master");
+        Response response = userResource.updateUser("Real user", "fake@user.com", "");
+        Assert.assertEquals(400, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void deleteUserPositiveTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+        
+        userResource.addUser("fake user", "fake@user.com", "admin,master");
+        Response response = userResource.deleteUser("fake@user.com");
+        Assert.assertEquals(200, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void deleteUserNotFoundNegativeTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+        
+        userResource.addUser("fake user", "fake@user.com", "admin,master");
+        Response response = userResource.deleteUser("dummy@user.com");
+        Assert.assertEquals(400, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void findUserPositiveTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+        
+        userResource.addUser("fake user", "fake@user.com", "admin,master");
+        Response response = userResource.findUser("fake@user.com");
+        Assert.assertEquals(200, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
+    }
+    
+    @Test
+    public void findUserNotFoundNegativeTest() {
+    	
+        userResource = new UserResource();
+        if (userDao == null) {
+            userDao = UserDao.getUserDao();
+        }
+        
+        userResource.addUser("fake user", "fake@user.com", "admin,master");
+        Response response = userResource.findUser("dummy@user.com");
+        Assert.assertEquals(400, response.getStatus());
+        
+        userResource=null;
+        userDao.users = null;
+        userDao = null;
     }
 }
